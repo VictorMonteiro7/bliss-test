9783d3868b85c13b4d85adb9779583026e56e050
Victor Monteiro <victormonteiro1995@gmail.com>
2023-03-19 21:37:13 -0300

    feat: added sass files and created some components

--- src/Views/Questions.vue
+++ src/Views/Questions.vue
@@ -1,36 +1,39 @@
-<script>
-import { computed } from 'vue';
+<script setup>
+import { computed, onMounted } from 'vue';
 import { useStore } from 'vuex';
+import QuestionItem from '@/components/QuestionItem.vue';
 
-export default {
-  name: 'questions',
-  setup() {
-    const store = useStore();
-    return {
-      questions: computed(() => store.getters.questions),
-      getQuestions: () => store.dispatch('getQuestions'),
-    };
-  },
-  mounted() {
-    if (!this.questions.length) {
-      this.getQuestions();
-    }
-  },
-};
+const store = useStore();
+const questions = computed(() => store.getters.questions);
+const getQuestions = () => store.dispatch('getQuestions');
+onMounted(() => {
+  if (!questions.length) {
+    getQuestions();
+  }
+});
 </script>
 
 <template>
-  <div id="questions">
+  <div
+    id="questions"
+    class="scrollbar w-100-percentage min-vw-40 max-vw-60"
+  >
     <div
       v-for="question in questions"
       v-bind:key="question.id"
     >
       <div class="">
-        {{ JSON.stringify(question) }}
+        <QuestionItem
+          v-bind:question="question"
+        />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+#questions {
+  margin: 0 auto;
+  overflow: scroll;
+}
 </style>
