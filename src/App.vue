3bbdc06a59f98701b4343d19845bd081ba38210f
Victor Monteiro <victormonteiro1995@gmail.com>
2023-03-20 22:42:33 -0300

    refactor: refact the App component, and created the Offline component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,49 @@
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router';
 import api from '@/api';
 import Loader from '@/components/Loader.vue';
-export default {
-  name: 'home',
-  setup() {
-    const isOnline = ref(navigator.onLine)
-    const isHealthy = ref(true);
-    const loading = ref(false);
-    const updateOnlineStatus = () => {
-      isOnline.value = navigator.onLine
-    };
-    window.addEventListener('online', updateOnlineStatus);
-    window.addEventListener('offline', updateOnlineStatus);
-    async function getHealthStats() {
-      loading.value = true;
-      try {
-        const health = await api.get('/health');
-        if (health.status !== 'OK') {
-          isHealthy.value = false;
-        } else {
-          isHealthy.value = true;
-          if (this.$route.name === 'questions' || this.$route.name === undefined) {
-            this.$router.push({ name: 'questions', query: { ...this.$route.query } });
-          }
-        }
-      } catch (err) {
-        isHealthy.value = false;
-      } finally {
-        loading.value = false;
-      }
-    };
-    return {
-      isOnline,
-      isHealthy,
-      getHealthStats,
-      loading,
-    };
-  },
-  components: { Loader },
-  mounted() {
-    this.getHealthStats();
-  },
+import Offline from '@/components/Offline.vue';
+let isOnline = ref(navigator.onLine);
+let isHealthy = ref(true);
+let loading = ref(false);
+const router = useRouter();
+const route = useRoute();
+const updateOnlineStatus = () => {
+  isOnline.value = navigator.onLine
 };
+window.addEventListener('online', updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);
+async function getHealthStats() {
+  loading.value = true;
+  try {
+    const health = await api.get('/health');
+    if (health.status !== 'OK') {
+      isHealthy.value = false;
+    } else {
+      isHealthy.value = true;
+      if (route.name === 'questions' || route.name === undefined) {
+        router.push({ name: 'questions', query: { ...route.query } });
+      }
+    }
+  } catch (err) {
+    isHealthy.value = false;
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => {
+  getHealthStats();
+});
+
+watch(isOnline, (value) => {
+  if (value) {
+    getHealthStats();
+  } else {
+    isHealthy.value = false;
+  }
+});
 </script>
 
 <template>
@@ -51,14 +52,15 @@ export default {
       <Loader />
     </template>
     <template v-if="!isOnline">
-      <div class="offline">
-        <h1>Offline</h1>
-        <button>It looks like you're offline. Please check your connection and try again.</button>
-      </div>
+      <Offline
+        v-on:@retry="getHealthStats"
+      />
     </template>
     <template v-else-if="isOnline && !isHealthy">
       <div class="offline">
-        <button>It looks like you're offline. Please check your connection and try again.</button>
+        <Offline
+          v-on:@retry="getHealthStats"
+        />
       </div>
     </template>
     <template v-else>
---
+++ src/components/Offline.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Button from './Button.vue';
+const emit = defineEmits(['@retry']);
+</script>
+
+<template>
+  <div class="d-flex align-center">
+    <h1 class="xxxl">😞</h1>
+    <div class="d-flex flex-column justify-center ml-2">
+      <h1 class="fs-xxxl text-primary">You are offline</h1>
+      <h1 class="fs-xxl text-primary">Please check your internet connection</h1>
+      <Button
+        text="Retry Connection"
+        v-on:@edit="emit('@retry')"
+      />
+    </div>
+  </div>
+</template>
